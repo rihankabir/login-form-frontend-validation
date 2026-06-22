822b11a87a1c80fba1b358909565719003929e65
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-06-22 20:53:22 +0600

    added js code for the form interictivity

--- js/form.js
+++ js/form.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+
     $("#loginfrm").submit((e) => {
         e.preventDefault();
+        let email = $("#email").val().trim();
+        let password = $("#password").val().trim();
+        let isValid = true;
+        $(".emailerror").text("");
+        $(".passworderror").text("");
+
+        if(email === ""){
+            $(".emailerror").text("Enter your Email").css("color", "red");
+            isValid = false;
+        }
+        if(password === ""){
+            $(".passworderror").text("Password required");
+            isValid = false;
+        }
     } )
 });
