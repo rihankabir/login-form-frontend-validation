b7b966ae462450b31b034512064cb1acf31281a9
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-06-22 22:49:57 +0600

    modified the js code and added email,password wrong message and color red and after successfull login the fields are empty

--- js/form.js
+++ js/form.js
@@ -13,8 +13,15 @@ $(document).ready(function () {
             isValid = false;
         }
         if(password === ""){
-            $(".passworderror").text("Password required");
+            $(".passworderror").text("Password required").css("color", "red");
             isValid = false;
+        }else if(password.length < 6){
+            $(".passworderror").text("password at least 6 characters").css("color", "red");
+            isValid = false;
+        }
+        if(isValid){
+            $(".successmsg").text("Login Successful").css("color", "green");
+            $("#email").val("")
         }
     } );
 });
